23f009618b2952516b1ed1b0d5cb112d569d72f1
gabrieldiaztoro <gdiaz_toro@hotmail.com>
2022-10-14 14:11:27 -0300

    criando 3 frames

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from tkinter import *
 from tkinter import Tk, StringVar, ttk
+from PIL import Image, ImageTk
 
 # importando cores
-cor01 = "#2e2d2b"  # Preto
-cor02 = "#feffff"  # Branco
+cor0 = "#2e2d2b"   # Preto
+cor01 = "#feffff"  # Branco
+cor02 = "#4fa882"  # verde
 cor03 = "#38576b"  # Verde
 cor04 = "#403d3d"
 cor05 = "#e06636"  # Profit
@@ -22,6 +24,20 @@ janela.resizable(width=FALSE, height=FALSE)
 
 style = ttk.Style(janela)
 style.theme_use("clam")
+
+#frame parte de cima
+frameCima = Frame(janela, width=1043, height=50, bg=cor01, relief=FLAT)
+frameCima.grid(row=0, column=0)
+
+#frame parte do meio
+frameMeio = Frame(janela, width=1043, height=303, pady=20, bg=cor01, relief=FLAT)
+frameMeio.grid(row=1, column=0, pady=1, padx=0, sticky=NSEW)
+
+#frame parte de baixo
+frameBaixo = Frame(janela, width=1043, height=300, pady=20, bg=cor01, relief=FLAT)
+frameBaixo.grid(row=2, column=0, pady=1, padx=1, sticky=NSEW)
+
+#Carregando Imagem
 
 
 janela.mainloop()
